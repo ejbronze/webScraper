866dee39192d6600e49268956151a6cfc7ff7cc4
Eddie Jaquez <erjaquez@gmail.com>
2019-06-06 21:45:33 -0400

    Set port dynamically for deploying to heroku

--- server.js
+++ server.js
@@ -86,6 +86,6 @@ app.get("/scrape", function(req, res) {
 });
 
 // Listen on port 3000
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("App running on port 3000!");
 });
